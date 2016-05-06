7f190c8c533505845c58e8b18a33c4544e42f269
cxpqwvtj <cxpqwvtj@gmail.com>
2016-05-07 07:50:11 +0900

    パス置換のため、sedコマンドの区切り文字変更

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 if [ ! -e 'pocci-account-center/public${APP_BASE_URL}/index.html' ]; then
 	cd /pocci-account-center
-	sed -i "s/var baseUrl = '';/var baseUrl = '${APP_BASE_URL}';/g" gulpfile.js
+	sed -i "s:var baseUrl = '';:var baseUrl = '${APP_BASE_URL}';:g" gulpfile.js
 	node ./node_modules/gulp/bin/gulp.js
 fi
 
